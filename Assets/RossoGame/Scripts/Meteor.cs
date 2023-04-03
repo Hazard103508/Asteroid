a34df9053e4bb6e88b8b5a5ea61bb31540452c71
Agustin Rosso <AgustinRosso_1@hotmail.com>
2023-04-03 11:19:09 -0300

    Se agrega funcionalidad de destruccion de meteorito

--- Assets/RossoGame/Scripts/Meteor.cs
+++ Assets/RossoGame/Scripts/Meteor.cs
@@ -1,17 +1,18 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
 namespace RossoGame
 {
     public class Meteor : MonoBehaviour
     {
+        public GameObject smallMeteorPref;
+
         private float speed;
         private float rotation;
         private Vector3 direction;
 
         private void Awake()
         {
-            speed = Random.Range(2f, 5f);
+            speed = Random.Range(1f, 3f);
             rotation = Random.Range(3, 90f);
             direction = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
         }
@@ -24,5 +25,26 @@ namespace RossoGame
 
         public void InvertX() => direction *= new Vector2(-1, 1);
         public void InvertY() => direction *= new Vector2(1, -1);
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.tag == "missile")
+            {
+                collision.gameObject.SetActive(false);
+                BreakMeteor();
+            }
+        }
+        private void BreakMeteor()
+        {
+            if (smallMeteorPref != null)
+                for (int i = 0; i < 2; i++)
+                {
+                    var obj = Instantiate(smallMeteorPref);
+                    obj.transform.position = this.transform.position;
+                }
+
+            Destroy(gameObject);
+        }
+
     }
 }
